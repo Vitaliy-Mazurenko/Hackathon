0e324f705ab376b5cb51ad60809496efa2e2b8d3
andriy66 <krol17aq@gmail.com>
2023-12-08 13:46:42 +0200

    Create telegram bot

--- back/src/main/java/com/hakaton/team1/helloworld/HelloWorldApplication.java
+++ back/src/main/java/com/hakaton/team1/helloworld/HelloWorldApplication.java
@@ -1,13 +1,18 @@
 package com.hakaton.team1.helloworld;
 
+import com.hakaton.team1.helloworld.controllers.BotController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class HelloWorldApplication {
-
-	public static void main(String[] args) {
+	private static TelegramBotsApi botsApi;
+	public static void main(String[] args) throws TelegramApiException {
 		SpringApplication.run(HelloWorldApplication.class, args);
+		botsApi = new TelegramBotsApi(DefaultBotSession.class);
+		botsApi.registerBot(new BotController());
 	}
-
 }
