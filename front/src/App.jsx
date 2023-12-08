1aca3ecfb2ab0d17bbd4ebb81476e7cad19d3aab
unknown <vit007m@gmail.com>
2023-12-08 15:23:36 +0200

    feat: created login page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,6 +2,7 @@ import {Navigate, Route, Routes} from "react-router-dom";
 import React from "react";
 
 const Home = React.lazy(() => import("./pages/home/Home.jsx"))
+const Users = React.lazy(() => import("./pages/users/Users.jsx"))
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
         <Routes>
             <Route path='*' element={<Navigate to='/home'/>}/>
             <Route path='/home' element={<Home/>}/>
+            <Route path='/users' element={<Users/>}/>
         </Routes>
     )
 }
--- front/src/pages/home/Home.jsx
+++ front/src/pages/home/Home.jsx
@@ -1,27 +1,97 @@
-import React from 'react';
-import {useDispatch, useSelector} from "react-redux";
+import { useState } from 'react';
+// import {useDispatch, useSelector} from "react-redux";
 import classes from './home.module.scss'
-import {decrement, increment} from "../../store/slices/countSlice.js";
-import {Button} from "@mui/material";
+// import {decrement, increment} from "../../store/slices/countSlice.js";
+import { useNavigate } from 'react-router-dom';
+import { Button } from "@mui/material";
+import { TextField } from "@mui/material";
 
 const Home = () => {
-    const count = useSelector(state => state.count.count)
-    const dispatch = useDispatch()
-    const handlePlus = () => {
-        dispatch(increment())
+    // const count = useSelector(state => state.count.count)
+    // const dispatch = useDispatch()
+    // const handlePlus = () => {
+    //     dispatch(increment())
+    // }
+    // const handleMinus = () => {
+    //     dispatch(decrement())
+    // }
+
+  const navigate = useNavigate();
+  const [login, setLogin] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordError, setPasswordError] = useState('')
+
+
+
+  const handleChangePassword = (e) => {
+    const value = e.target.value;
+    setPassword(value.trim())
+    setPasswordError('');
+  }
+
+  const handleBlurPassword = () => {
+    const passwordValid = password.length >= 6;
+    setPasswordError(passwordValid ? '' : 'Password is too short');
+  }
+
+  const handleSubmit = async () => {
+    if (password) {   
+    navigate('/users');
     }
-    const handleMinus = () => {
-        dispatch(decrement())
-    }
+
+  }
+
     return (
         <div className={classes.home}>
-            <Button onClick={handleMinus}>
-                -
-            </Button>
-            <h1>{count}</h1>
-            <Button onClick={handlePlus}>
-                +
-            </Button>
+             <div className="App-header">
+         <h2 className="text-header">{/*е-Ветеран*/}</h2> 
+      </div>
+      <form className="demoForm" style={{ width: '500px', margin: 'auto' }}>
+        <h2 className='textForm'>Login</h2>
+        <div className='form-group'>
+          <TextField
+            id="outlined-basic"
+            label="login"
+            variant="outlined"
+            value={login}
+            onChange={(e) => setLogin(e.target.value)}
+            className="form-control" name="login"
+            // helperText={loginError}
+            // error={!!loginError}
+          />
+        </div>
+        <div className='form-group'>
+          <TextField
+            id="outlined-password-input"
+            type="password"
+            label="Password"
+            variant="outlined"
+            value={password}
+            onChange={handleChangePassword}
+            onBlur={handleBlurPassword}
+            className="form-control" name="password"
+            helperText={passwordError}
+            error={passwordError}
+          />
+        </div>
+        <Button className='buttonLogin'
+          variant="contained"
+          color="primary"
+          disabled={passwordError}
+          onClick={handleSubmit}>Login
+        </Button>
+
+        {/* {loginError ? (
+          <div style={{
+            textAlign: 'start',
+            marginTop: 30,
+            paddingLeft: '25px',
+            color: 'red',
+          }}>
+            - {loginError}
+          </div>
+        ) : null} */}
+      </form>
         </div>
     );
 };
