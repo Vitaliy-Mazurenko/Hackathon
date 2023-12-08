4b899ac7d13ef8e3a1e24e257919e7b35d1f4b43
Vadim Zaloznyj <vadimmroz@gmail.com>
2023-12-08 23:52:30 +0200

    vadim 2

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,21 +1,22 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import React from "react";
 import Header from "./components/Header/Header.jsx";
 import Sidebar from "./components/Sidebar/Sidebar.jsx";
-
-const Home = React.lazy(() => import("./pages/home/Home.jsx"));
+import Question from "./pages/question/Question.jsx";
+import Home from "./pages/home/Home.jsx"
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Sidebar />
-      <Routes>
-        <Route path="*" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            <Header/>
+            <Sidebar/>
+            <Routes>
+                <Route path="*" element={<Navigate to="/home"/>}/>
+                <Route path="/home" element={<Home/>}/>
+                <Route path='/question' element={<Question/>}/>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
--- front/src/components/Sidebar/Sidebar.jsx
+++ front/src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import {useState} from "react";
 import style from "./Sidebar.module.scss";
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import Collapse from "@mui/material/Collapse";
 
 import message from "../../assets/message.svg";
@@ -9,55 +9,55 @@ import analitycs from "../../assets/analitycs.svg";
 import arrow from "../../assets/arrow.svg";
 
 function Sidebar() {
-  const [open, setOpen] = useState(true);
-  const handleClick = () => {
-    setOpen(!open);
-  };
-  return (
-    <div className={style.sidebar}>
-      <div
-        className={`${style.navLink} ${style.dropdown}`}
-        onClick={handleClick}
-      >
+    const [open, setOpen] = useState(true);
+    const handleClick = () => {
+        setOpen(!open);
+    };
+    return (
+        <div className={style.sidebar}>
+            <div
+                className={`${style.navLink} ${style.dropdown}`}
+                onClick={handleClick}
+            >
         <span>
-          <img className={style.linkIcon} src={message} alt="" />
+          <img className={style.linkIcon} src={message} alt=""/>
           Чат-бот
         </span>
-        <div className={style.dropdownBtn}>
-          <img className={` ${open ? style.rotate : ""}`} src={arrow} alt="" />
-        </div>
-      </div>
-      <Collapse
-        className={style.dropdownList}
-        in={open}
-        timeout="auto"
-        unmountOnExit
-      >
-        <NavLink to="/dassd" className={style.navLink}>
-          <span>Форма Заяви</span>
-        </NavLink>
-        <NavLink to="/home" className={style.navLink}>
-          <span>Питання-Відповіді</span>
-        </NavLink>
-        <NavLink to="/asd" className={style.navLink}>
-          <span>Розсилка</span>
-        </NavLink>
-      </Collapse>
-      <NavLink to="/application" className={style.navLink}>
+                <div className={style.dropdownBtn}>
+                    <img className={` ${open ? style.rotate : ""}`} src={arrow} alt=""/>
+                </div>
+            </div>
+            <Collapse
+                className={style.dropdownList}
+                in={open}
+                timeout="auto"
+                unmountOnExit
+            >
+                <NavLink to="/form" className={style.navLink}>
+                    <span>Форма Заяви</span>
+                </NavLink>
+                <NavLink to="/question" className={style.navLink}>
+                    <span>Питання-Відповіді</span>
+                </NavLink>
+                <NavLink to="/mailing" className={style.navLink}>
+                    <span>Розсилка</span>
+                </NavLink>
+            </Collapse>
+            <NavLink to="/application" className={style.navLink}>
         <span>
-          <img className={style.linkIcon} src={contacts} alt="" />
-          Аналітика
+          <img className={style.linkIcon} src={contacts} alt=""/>
+        Заяви на Проєкт
         </span>
-      </NavLink>
+            </NavLink>
 
-      <NavLink to="/analitycs" className={style.navLink}>
+            <NavLink to="/analyst" className={style.navLink}>
         <span>
-          <img className={style.linkIcon} src={analitycs} alt="" />
-          Налаштування Боту
+          <img className={style.linkIcon} src={analitycs} alt=""/>
+            Аналітика
         </span>
-      </NavLink>
-    </div>
-  );
+            </NavLink>
+        </div>
+    );
 }
 
 export default Sidebar;
