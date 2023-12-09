2fb8cb8315983076a0404cdb569ae734184123dd
Vadim Zaloznyj <vadimmroz@gmail.com>
2023-12-09 11:31:09 +0200

    vadim 4

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,26 +1,30 @@
 import {Navigate, Route, Routes} from "react-router-dom";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "./components/Header/Header.jsx";
 import Sidebar from "./components/Sidebar/Sidebar.jsx";
 import Question from "./pages/question/Question.jsx";
 import Home from "./pages/home/Home.jsx"
+import axios from "axios";
+import Analyst from "./pages/analyst/Analyst.jsx";
 
 function App() {
-    const login = localStorage.getItem('login')
+   const [login, setLogin] = useState(localStorage.getItem('login'))
+
 
     return (
         <>
             <Header/>
             <Sidebar/>
-            {!(login?.length > 1) ?
+            {!(login?.length > 0) ?
                 <Routes>
                     <Route path="*" element={<Navigate to="/home"/>}/>
                     <Route path="/home" element={<Home/>}/>
                     <Route path='/question' element={<Question/>}/>
+                    <Route path='/analyst' element={<Analyst/>}/>
                 </Routes> :
                 <Routes>
                     <Route path="*" element={<Navigate to="/login"/>}/>
-                    <Route path="/login" element={<Home/>}/>
+                    <Route path="/login" element={<Login setLogin={setLogin}/>}/>
                 </Routes>
             }
         </>
