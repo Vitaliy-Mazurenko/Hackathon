405f0bba4f7763abffe8c5fb238994fa22538b31
Vadim Zaloznyj <vadimmroz@gmail.com>
2023-12-09 09:31:28 +0200

    vadim 3

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -6,15 +6,23 @@ import Question from "./pages/question/Question.jsx";
 import Home from "./pages/home/Home.jsx"
 
 function App() {
+    const login = localStorage.getItem('login')
+
     return (
         <>
             <Header/>
             <Sidebar/>
-            <Routes>
-                <Route path="*" element={<Navigate to="/home"/>}/>
-                <Route path="/home" element={<Home/>}/>
-                <Route path='/question' element={<Question/>}/>
-            </Routes>
+            {!(login?.length > 1) ?
+                <Routes>
+                    <Route path="*" element={<Navigate to="/home"/>}/>
+                    <Route path="/home" element={<Home/>}/>
+                    <Route path='/question' element={<Question/>}/>
+                </Routes> :
+                <Routes>
+                    <Route path="*" element={<Navigate to="/login"/>}/>
+                    <Route path="/login" element={<Home/>}/>
+                </Routes>
+            }
         </>
     );
 }
